6886c574652626fa10cdb1c4e803436210eba95a
Enzo Turpin <enzoturpin35@orange.fr>
2024-08-29 23:22:53 +0200

    Changement des noms des photos

--- script.js
+++ script.js
@@ -74,3 +74,26 @@ function updateLanguageList(selectedLang) {
     }
   }
 }
+
+// Afficher le bouton lorsque l'utilisateur fait défiler vers le bas de 100px
+window.onscroll = function () {
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    scrollToTopBtn.style.display = "flex"; // Utilise flex pour centrer le contenu
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+// Remonter en haut de la page lorsqu'on clique sur le bouton
+document
+  .getElementById("scrollToTopBtn")
+  .addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
